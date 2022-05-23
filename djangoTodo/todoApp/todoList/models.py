90ba14495b7c4fa9d05d3bdcdc03a7031cfd7dc4
leandra-gg <101011803+leandra-gg@users.noreply.github.com>
2022-05-23 12:13:58 +0200

    Update models.py

--- djangoTodo/todoApp/todoList/models.py
+++ djangoTodo/todoApp/todoList/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Liste(models.Mode):
+class List(models.Model):
     taskText = models.CharField
     taskDate = models.DateField (null=True)
     taskDerc = models.IntegerField(null=True)

83f8f69e2b58763185632ccb73bf6ec36210026b
armand143 <armandborel@yahoo.fr>
2022-05-23 12:09:29 +0200

    final 2

--- djangoTodo/todoApp/todoList/models.py
+++ djangoTodo/todoApp/todoList/models.py
@@ -1,7 +1,4 @@
 from django.db import models
 
-class List(models.Model):
-    taskText = models.CharField(max_length=150, null=True)
-    taskDate = models.DateField (null=True)
-    taskDerc = models.IntegerField(null=True)
-   
+class Liste(models.Mode):
+    taskText = models.CharField

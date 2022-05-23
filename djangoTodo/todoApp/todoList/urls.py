6cf3fa4f5aa16bcc1ea0626b41e2c1a51899f86e
leandra-gg <101011803+leandra-gg@users.noreply.github.com>
2022-05-23 10:51:54 +0200

    final changes

--- djangoTodo/todoApp/todoList/urls.py
+++ djangoTodo/todoApp/todoList/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('newTodo/', views.newTodo, name= 'newTodo'),
     path('impressum/', views.impressum, name = 'impressum'),
-    path('list/',views.list,name='list'),
     path('delete/<int:id>', views.delete, name = 'delete'),
     path('edit/<int:id>', views.edit, name = 'edit'),
     path('submit',views.submit,name='submit'), 
--- djangoTodo/todoApp/todoList/views.py
+++ djangoTodo/todoApp/todoList/views.py
@@ -16,26 +16,6 @@ def home(request):
     }
     return render(request,'home.html',context=mydict)
 
-# def submit(request): 
-#     obj = List()
-#     obj.text = request.GET['text']
-#     obj.date = request.GET['date']
-#     obj.perc = request.GET['perc']
-#     obj.save()
-#     dic = {
-#         "allTodos" : List.objects.all()
-#     }
-#     return render(request, 'list.html', context=dic)
-
-# def addTodo(request):
-#     todo_list = List.objects.order_by('id')
-#     form = ListForm()
-
-
-#     context = {'todo_list' : todo_list, 'form' : form}
-
-#     return render(request, 'home.html', context)
-
 
 def submit(request):
     obj = List()
@@ -43,16 +23,14 @@ def submit(request):
             obj.taskText = request.GET.get("taskText")
             obj.taskDate = request.GET.get("taskDate")
             obj.taskDerc = request.GET.get("taskDerc")
-            # obj.taskText = request.POST['taskText']
-            # obj.taskDate = request.POST['taskDate']
-            # obj.taskDerc = request.POST['taskDerc']
-            obj.save()#
+            
+            obj.save()
             mydict = {
                 "all_tasks" : List.objects.all()
             }
             return redirect(home)
 
-            #return render(request, 'home.html', context=mydict)
+        
     
     else:
         mydict = {
@@ -61,37 +39,12 @@ def submit(request):
         return render(request, 'home.html', context=mydict)
 
 
-# def newTodo(request):
-#     if request.method == 'POST':
-#          taskText = request.POST.get('taskText')
-#          taskDate = request.POST.get('taskDate')
-#          taskDerc = request.POST.get('taskDerc')
-#         #  form = ListForm(request.POST or None)
-
-#          all_tasks = List(taskText=taskText, taskDate = taskDate, taskDerc = taskDerc)
-#          all_tasks.save()
-#          if all_tasks.is_valid():
-#              all_tasks.save()
-#             #  all_tasks=List.objects.all
-             
-#          return render(request, 'newTodo.html', {'all_tasks': all_tasks})
-
-#     else:
-#          all_tasks = List.objects.all
-#          return render (request, 'newTodo.html', {'all_tasks': all_tasks})
-
-
 def impressum (request):
     return render(request, "impressum.html")
 
 def newTodo (request):
     return render(request, "newTodo.html")
 
-def list(request):
-    mydict = {
-        "all_tasks" : List.objects.all()
-    }
-    return render(request,'list.html',context=mydict)
 
 def edit(request,id):
     obj = List.objects.get(id=id)
